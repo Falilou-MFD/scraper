ee2cc4b87e8de3960f9ed06d1a8e4dcbc5b3b68a
Falilou-MFD <157077834+Falilou-MFD@users.noreply.github.com>
2025-02-17 18:53:45 +0000

    Update Projet_Streamlit.py

--- Projet_Streamlit.py
+++ Projet_Streamlit.py
@@ -13,7 +13,7 @@ menu = st.sidebar.radio("Navigation", ["Scraper en temps réel", "Télécharger 
 def scrape_shoes(pages, category):
     data = []
     base_url = "https://sn.coinafrique.com/categorie/chaussures-"
-    url = base_url + ("homme" if category == "hommes" else "enfants")
+    url = base_url + ("Homme" if category == "Hommes" else "Enfants")
     
     for page in range(1, pages + 1):
         res = requests.get(f"{url}?page={page}")
@@ -26,15 +26,15 @@ def scrape_shoes(pages, category):
                 type_ = container.find("p", class_="ad__card-description").text.strip()
                 location = container.find("p", class_="ad__card-location").span.text.strip()
                 img_url = container.find("img", class_="ad__card-img")["src"]
-                data.append({"Prix": price, "Type": type_, "Localisation": location, "Image": img_url})
+                data.append({"Prix": price, "Type": type_, "Adresse": location, "Image": img_url})
             except:
                 pass
     return pd.DataFrame(data)
 
 if menu == "Scraper en temps réel":
     st.title("📌 Scraping en temps réel")
-    pages = st.number_input("Nombre de pages à scraper", min_value=1, max_value=10, value=2)
-    category = st.radio("Choisissez la catégorie :", ["hommes", "enfants"], horizontal=True)
+    pages = st.number_input("Nombre de pages à scraper", min_value=1, max_value=200, value=2)
+    category = st.radio("Choisissez la catégorie :", ["Hommes", "Enfants"], horizontal=True)
     if st.button("Lancer le scraping"):
         with st.spinner("Scraping en cours..."):
             df = scrape_shoes(pages, category)
@@ -48,8 +48,8 @@ if menu == "Scraper en temps réel":
 
 elif menu == "Télécharger les données":
     st.title("📂 Télécharger un fichier CSV")
-    category_download = st.radio("Télécharger les données pour :", ["hommes", "enfants"], horizontal=True)
-    file_path = "data/chaussures_hommes_ws.csv" if category_download == "hommes" else "data/chaussures_enfants_ws.csv"
+    category_download = st.radio("Télécharger les données pour :", ["Hommes", "Enfants"], horizontal=True)
+    file_path = "data/chaussures_hommes_ws.csv" if category_download == "Hommes" else "data/chaussures_enfants_ws.csv"
     
     if st.button("Afficher les données"):
         df_download = pd.read_csv(file_path)
@@ -60,12 +60,12 @@ elif menu == "Télécharger les données":
 
 elif menu == "Formulaire KoboToolbox":
     st.title("📝 Formulaire KoboToolbox")
-    st.markdown("[Remplir le formulaire KoboToolbox]")
+    st.markdown("Veuillez remplir ce formulaire pour evaluer l'application")
     components.html(""" <iframe src="https://ee.kobotoolbox.org/x/KB1xDzPa" width="800" height="1100"></iframe>
     """,height=1100,width=800)
 
 elif menu == "Formulaire Google Forms":
     st.title("📝 Formulaire Google Forms")
-    st.markdown("[Remplir le formulaire Google Forms]")
+    st.markdown("Veuillez remplir ce formulaire pour evaluer l'application")
     components.html(""" <iframe src="https://forms.gle/CJvrNLFWgyCJZTBg6" width="800" height="1100"></iframe>
     """,height=1100,width=800)
